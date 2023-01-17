cd6c67f63053774b4eea37ce0aadc1d9d2d8f48b
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-01-17 15:51:07 +0530

    Update Home.js

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -9,7 +9,6 @@ import { Link } from "react-router-dom";
 export default function Home(){
 
     const params=useParams();
-
     const navigate=useNavigate();
     return(
         <div className="home-body">
