9aa859565c61927ca25f7faed7eb0aefce99aa8e
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-01-17 20:23:50 +0530

    Update ResultComponent.js

--- frontend/src/components/ResultComponent.js
+++ frontend/src/components/ResultComponent.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { Navigate, renderMatches, useNavigate } from 'react-router-dom';
 // import Form from 'react-bootstrap/Form';
 import {render} from 'react-dom';
-import "./leaderboard.css";
 import axios from 'axios'; 
+import "./result.css";
 import { useEffect,useState } from 'react'; 
 
 
@@ -11,22 +11,15 @@ export default function ResultComponent(props)
 {
 
     const navigate= useNavigate();
-   
-   
   return (
-
-    <tbody>
-    <tr>
-      <td>{props.questnum}</td>
-      <td>{props.time}</td>
-      <td>
-        <div class="result-box">{props.passed}</div>
-      </td>
-    </tr>
-   
-    </tbody>
- 
-        
+        <tbody>
+        <tr>
+          <td>{props.questnum}</td>
+          <td>
+            <div class="result-box">{props.passed}</div>
+          </td>
+        </tr>
+        </tbody> 
     )
     
 }
