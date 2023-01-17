401af7fedcbb1735f8fe6514f1ad052f4081b8c4
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-01-17 20:22:51 +0530

    Update Result.js

--- frontend/src/screens/Result.js
+++ frontend/src/screens/Result.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { Navigate, renderMatches, useNavigate } from 'react-router-dom';
-// import Form from 'react-bootstrap/Form';
 import {render} from 'react-dom';
 
 import axios from 'axios'; 
@@ -18,25 +17,11 @@ export default function Result()
         const fetchData=async()=>{
             const result=await axios.get('/api/users/result');
             setResult(result.data);
-        
-        // e.preventDefault();
-        // try{
-        //   const quest=await Axios.get('/api/users/quest');
-        //   const [questobj]=quest.data;
-        //   console.log(quest.data)
-        //   console.log(questobj);
-        //   setQuestion(quest.data)
-         
-          
-        //    navigate(`/home/${params.name}/quest`);
-        // }
-        // catch(err){
-        //   console.log(err);
-        // }
-        
       };
       fetchData();
     },[]);
+    const newresult=result.slice(0,3);
+    const res=result.slice(3);
    
   return (
 
@@ -46,53 +31,37 @@ export default function Result()
         <li><a href="/leaderboard">Leaderboard</a></li>
     </ul>
 <div className="result-block" style={{display:'flex'}}>
-<div class="myblock">
+<div class="myblock1">
+<h2>Overall Analysis</h2>
+{res.map((q)=>{
+    return(
+      <div>
+      <p className="timeDiv1">Total time: {q.time}</p>
+      <p className="timeDiv1">Alloted time: 3:00:00</p>
+      </div>
+    )
+  })}
 <table>
 <thead>
   <tr>
     <th>Questions</th>
-    <th>Time</th>
     <th>Passed</th>
   </tr>
 </thead>
- {result.map((q)=>{
+ {newresult.map((q)=>{
      console.log(q);
  return(
      <ResultComponent
  questnum={q.questnum}
- time={q.time}
  passed={q.passed}
  />
  );
  }
  )}
-
-
   </table>
 </div>
 </div>
 </div>
-    
     )
     
 }
-
-
-
-// {leaderboard.map((q)=>{
-//     console.log(q);
-// return(
-//     <LeaderboardComponent 
-// rank={q.rank}
-// name={q.name}
-// finishtime={q.finishtime}
-// />
-// );
-// }
-
-
-
-// )}
-
-
-
