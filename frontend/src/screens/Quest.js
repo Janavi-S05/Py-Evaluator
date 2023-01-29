4dd00254e817a645be601e732f1b8413031882e0
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-01-29 14:32:13 +0530

    Update Quest.js

--- frontend/src/screens/Quest.js
+++ frontend/src/screens/Quest.js
@@ -4,18 +4,45 @@ import { Navigate, renderMatches, useNavigate ,Link, useParams} from 'react-rout
 import { useEffect,useState } from 'react';
 import "./quest.css";
 import QuestComponent from '../components/QuestComponent';
+import { useContext } from 'react';
+
+
+import { Store } from '../Store';
 export default function Quest(props)
 {
 
     const navigate= useNavigate();
-    const[question,setQuestion]= useState([])
+    const[question,setQuestion]= useState([]);
     const params=useParams();
-    
+    const {state , dispatch:ctxDispatch}=useContext(Store);
+    const {userInfo}=state;
+    const signoutHandler=()=>{
+        ctxDispatch({type:'DELETE_USERINFO'});
+        localStorage.removeItem('userInfo');
+       
+        window.location.href='/';
+      }
     useEffect(()=>{
 
-            const fetchData=async()=>{
+                const fetchData=async()=>{
                 const result=await axios.get('/api/users/quest');
                 setQuestion(result.data);
+            
+            // e.preventDefault();
+            // try{
+            //   const quest=await Axios.get('/api/users/quest');
+            //   const [questobj]=quest.data;
+            //   console.log(quest.data)
+            //   console.log(questobj);
+            //   setQuestion(quest.data)
+             
+              
+            //    navigate(`/home/${params.name}/quest`);
+            // }
+            // catch(err){
+            //   console.log(err);
+            // }
+            
           };
           fetchData();
         },[]);
@@ -29,7 +56,8 @@ export default function Quest(props)
         <div class="item quest-header">
             <h3 class="quest-header">Solved: 0/3</h3>
             <h3 class="quest-header"><Link to={`/home/${params.name}/leaderboard`} className='ques-board'>Leaderboard</Link></h3>
-            <h3 class="quest-header">Profile</h3>
+            {/* <img src={require('./change-icon.png')} alt="img" onClick={handleThemeChange}/> */}
+            <button class="logout quest-header"onClick={signoutHandler}>Logout</button>
         </div>
         <div className="item sidebar">
             <div id="hover"><a href="/quest"><p class="num">Quests</p></a></div>
@@ -53,10 +81,19 @@ export default function Quest(props)
             />
             );
         }
+           
+           
+
         )}
+                {/* </div> */}
+
+                
+                
             </div>
             
-        </div> 
+        </div>
+        
+        
     </div>
     </div>
     )
