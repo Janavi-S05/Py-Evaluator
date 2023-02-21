5d83fab517765f973150ba843e1c7ffa0a505f86
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-02-21 12:35:12 +0530

    Create Verify.js

---
+++ frontend/src/screens/Verify.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Camera from '../components/Camera';
+const Verify= () => {
+
+  return (
+   <div>
+    <Camera/>
+
+    
+   </div>
+  );
+};
+
+
+export default Verify;
